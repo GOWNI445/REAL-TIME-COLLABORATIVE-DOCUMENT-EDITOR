a0f765da0af42d2a01245da6ecb59df2f1191489
GOWNI445 <indugowni@gmail.com>
2025-06-03 22:51:46 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('text-change', (data) => {
+    socket.broadcast.emit('text-update', data); // Send to all except sender
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
